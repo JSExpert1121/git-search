6e6e969902a1243b1bd8f7eb55c0bf4d835afe33
JSExpert1121 <dage840128@outlook.com>
2021-08-04 22:56:27 +0300

    root folders restructured

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import 'assets/styles/index.scss'
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>
+				Hello world
+			</h1>
+    </div>
+  );
+}
+
+export default App;
